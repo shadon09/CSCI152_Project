83cd1c42e9cd113d140cc16399dfec8537859acf
Matt <matthewmorado@gmail.com>
2017-02-27 19:44:46 -0800

    Refactoring and Added Title Screen

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,16 +1,14 @@
-
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
-
-function preload() {
+var boxes;
+var mainState = {
+preload: function(){
   this.game.load.spritesheet('player', 'assets/images/dude.png', 32, 48);
 	this.game.load.tilemap('tilemap', 'assets/level1.json', null, Phaser.Tilemap.TILED_JSON);
   this.game.load.image('tiles', 'assets/images/Tiles_64x64.png');
 	this.game.load.image('box', 'assets/images/tile_06.png');
-}
+},
 
-var boxes
 
-function create() {
+create: function(){
 	//Start the Arcade Physics systems
 	this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
@@ -59,12 +57,12 @@ function create() {
 
 	//Enable cursor keys so we can create some controls
 	this.cursors = this.game.input.keyboard.createCursorKeys();
-}
+},
 
-function update() {
+update: function() {
 	//Make the sprite collide with the ground layer
 	this.game.physics.arcade.collide(this.sprite, this.groundLayer);
-	this.game.physics.arcade.collide(this.sprite, boxes, destroyBox);
+	this.game.physics.arcade.collide(this.sprite, boxes, this.destroyBox);
 	//Make the sprite jump when the up key is pushed
     if(this.cursors.up.isDown && this.sprite.body.blocked.down) {
       this.sprite.body.velocity.y = -1000;
@@ -79,77 +77,42 @@ function update() {
 		else {
 			this.sprite.body.velocity.x = 0;
 		}
+},
+
+destroyBox: function(sprite, box) {
+	box.destroy();
+},
 }
 
-function destroyBox(sprite, box) {
-	box.destroy();
-}
-/*jumpTimer = 0;
-var mainState = {
+
+var titleState = {
 	preload: function(){
-		//Load image assets
-		game.load.image('player', '/assets/images/ph_char.png');
-		game.load.image('block', '/assets/images/ph_block.png')
+		game.stage.backgroundColor = '#000000';
 	},
 
 	create: function(){
-		game.stage.backgroundColor = '#FFFFFF';
-		//Start the physics system
-		game.physics.startSystem(Phaser.Physics.ARCADE);
-
-		//Create the player, enable its physics events, and set its gravity
-		this.player = game.add.sprite(game.world.centerX,game.world.centerY,'player');
-		game.physics.arcade.enable(this.player);
-		this.player.body.gravity.y = 250;
-
-		//Create a sprite group that will contain blocks for the flooring
-		this.blocks = game.add.group();
-		this.blocks.enableBody = true;
-		this.blocks.physicsBodyType = Phaser.Physics.ARCADE;
-		this.blocks.createMultiple(30, 'block');
-		this.blocks.setAll('anchor.x', 0.5);
-		this.blocks.setAll('anchor.y', 1);
-		this.blocks.setAll('outOfBoundsKill', true);
-		this.blocks.setAll('checkWorldBounds', true);
-
-		this.createBlocks();	
+		this.labelTitle = game.add.text(game.world.centerX, game.world.centerY-125, "Platformer", {font: '50px Arial', fill: '#ffffff'});
+		this.labelTitle.anchor.setTo(0.5, 0.5);
+		this.labelTitle = game.add.text(game.world.centerX, game.world.centerY-25, "Play", {font: '30px Arial', fill: '#ffffff'});
+		this.labelTitle.anchor.setTo(0.5, 0.5);
+		this.labelTitle.inputEnabled = true;
+		this.labelTitle.events.onInputDown.add(function(){
+			game.state.start('main');
+		}, this);
+		var spaceKey = game.input.keyboard.addKey(
+			Phaser.Keyboard.SPACEBAR);
+		spaceKey.onDown.add(function(){
+			game.state.start('main');
+		}, this);
 	},
 
 	update: function(){
-		this.playerControl();
-		//Check for collisions against the player and the blocks
-		game.physics.arcade.collide(this.player, this.blocks);
-	},
 
-	playerControl: function(){
-		//Left and Right keys make the player go left and right
-		if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
-			this.player.body.velocity.x = -200;
-		}
-		else if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
-			this.player.body.velocity.x = 200;
-		}
-		else{
-			this.player.body.velocity.x = 0;
-		}
-		//Spacebar makes the player jump
-		//Can only jump when collided with the blocks
-		if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR) && game.physics.arcade.collide(this.player, this.blocks) && game.time.now > jumpTimer){
-			this.player.body.velocity.y = -250;
-        	jumpTimer = game.time.now + 750;
-		}
-	},
-
-	//Creates the flooring
-	createBlocks: function(){
-		for(var x = 1; x < 31; x++){
-			this.block = this.blocks.create(x*32, game.world.height-32, 'block');
-			this.block.anchor.setTo(0.5, 0.5);
-			this.block.body.immovable = true;
-		}
 	},
 }
 
+
 var game = new Phaser.Game(800,600);
 game.state.add('main', mainState);
-game.state.start('main');*/
+game.state.add('title', titleState);
+game.state.start('title');
