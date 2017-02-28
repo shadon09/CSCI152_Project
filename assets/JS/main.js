88f0903e47c0982535426a965bc4c1e5b4e37733
Matt <matthewmorado@gmail.com>
2017-02-27 20:15:25 -0800

    Refactoring Documentation

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,73 +1,73 @@
-var boxes;
+//Put the entire state into a variable
 var mainState = {
-preload: function(){
-  this.game.load.spritesheet('player', 'assets/images/dude.png', 32, 48);
-	this.game.load.tilemap('tilemap', 'assets/level1.json', null, Phaser.Tilemap.TILED_JSON);
-  this.game.load.image('tiles', 'assets/images/Tiles_64x64.png');
-	this.game.load.image('box', 'assets/images/tile_06.png');
-},
+	preload: function(){
+  		this.game.load.spritesheet('player', 'assets/images/dude.png', 32, 48);
+			this.game.load.tilemap('tilemap', 'assets/level1.json', null, Phaser.Tilemap.TILED_JSON);
+ 	 	this.game.load.image('tiles', 'assets/images/Tiles_64x64.png');
+			this.game.load.image('box', 'assets/images/tile_06.png');
+	},
 
 
-create: function(){
-	//Start the Arcade Physics systems
-	this.game.physics.startSystem(Phaser.Physics.ARCADE);
+	create: function(){
+		//Start the Arcade Physics systems
+		this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
-	//Change the background colour
-	this.game.stage.backgroundColor = "#a9f0ff";
+		//Change the background colour
+		this.game.stage.backgroundColor = "#a9f0ff";
 
-	//Add the tilemap and tileset image. The first parameter in addTilesetImage
-	//is the name you gave the tilesheet when importing it into Tiled, the second
-	//is the key to the asset in Phaser
-	this.map = this.game.add.tilemap('tilemap');
-	this.map.addTilesetImage('Tiles_64x64', 'tiles');
+		//Add the tilemap and tileset image. The first parameter in addTilesetImage
+		//is the name you gave the tilesheet when importing it into Tiled, the second
+		//is the key to the asset in Phaser
+		this.map = this.game.add.tilemap('tilemap');
+		this.map.addTilesetImage('Tiles_64x64', 'tiles');
 
-	//Add both the background and ground layers. We won't be doing anything with the
-	//GroundLayer though
-	this.backgroundlayer = this.map.createLayer('Background');
-	this.groundLayer = this.map.createLayer('Ground');
+		//Add both the background and ground layers. We won't be doing anything with the
+		//GroundLayer though
+		this.backgroundlayer = this.map.createLayer('Background');
+		this.groundLayer = this.map.createLayer('Ground');
 
-	//Before you can use the collide function you need to set what tiles can collide
-	this.map.setCollisionBetween(1, 100, true, 'Ground');
+		//Before you can use the collide function you need to set what tiles can collide
+		this.map.setCollisionBetween(1, 100, true, 'Ground');
 
-	//Add the sprite to the game and enable arcade physics on it
-	this.sprite = this.game.add.sprite(50, this.game.world.centerY, 'player');
-	this.game.physics.arcade.enable(this.sprite);
+		//Add the sprite to the game and enable arcade physics on it
+		this.sprite = this.game.add.sprite(50, this.game.world.centerY, 'player');
+		this.game.physics.arcade.enable(this.sprite);
 
-	boxes = this.game.add.group();
-	boxes.enableBody = true;
-	boxes.setAll('immovable',true);
-	boxes.setAll('body.moves', false);
+		this.boxes = this.game.add.group();
+		this.boxes.enableBody = true;
+		this.boxes.setAll('immovable',true);
+		this.boxes.setAll('body.moves', false);
 
-	this.map.createFromObjects('Object Layer 1', 7, 'box', 0, true, false, boxes);
-	//Change the world size to match the size of this layer
-	this.groundLayer.resizeWorld();
+		this.map.createFromObjects('Object Layer 1', 7, 'box', 0, true, false, this.boxes);
+		//Change the world size to match the size of this layer
+		this.groundLayer.resizeWorld();
 
-	//Set some physics on the sprite
-	this.sprite.body.bounce.y = 0.2;
-	this.sprite.body.gravity.y = 2000;
-	this.sprite.body.gravity.x = 20;
-	this.sprite.body.velocity.x = 0;
+		//Set some physics on the sprite
+		this.sprite.body.bounce.y = 0.2;
+		this.sprite.body.gravity.y = 2000;
+		this.sprite.body.gravity.x = 20;
+		this.sprite.body.velocity.x = 0;
 
-	//Create a running animation for the sprite and play it
-	this.sprite.animations.add('right', [5, 6, 7, 8], 10, true);
-	this.sprite.animations.play('right');
+		//Create a running animation for the sprite and play it
+		this.sprite.animations.add('right', [5, 6, 7, 8], 10, true);
+		this.sprite.animations.play('right');
 
-	//Make the camera follow the sprite
-	this.game.camera.follow(this.sprite);
+		//Make the camera follow the sprite
+		this.game.camera.follow(this.sprite);
 
-	//Enable cursor keys so we can create some controls
-	this.cursors = this.game.input.keyboard.createCursorKeys();
-},
+		//Enable cursor keys so we can create some controls
+		this.cursors = this.game.input.keyboard.createCursorKeys();
+	},
 
-update: function() {
-	//Make the sprite collide with the ground layer
-	this.game.physics.arcade.collide(this.sprite, this.groundLayer);
-	this.game.physics.arcade.collide(this.sprite, boxes, this.destroyBox);
-	//Make the sprite jump when the up key is pushed
-    if(this.cursors.up.isDown && this.sprite.body.blocked.down) {
-      this.sprite.body.velocity.y = -1000;
-    }
-
+	update: function() {
+		//Make the sprite collide with the ground layer
+		this.game.physics.arcade.collide(this.sprite, this.groundLayer);
+		this.game.physics.arcade.collide(this.sprite, this.boxes, this.destroyBox);
+		//Make the sprite jump when the up key is pushed
+    		if(this.cursors.up.isDown && this.sprite.body.blocked.down) {
+      			this.sprite.body.velocity.y = -1000;
+    		}
+	
 		if(this.cursors.right.isDown) {
 			this.sprite.body.velocity.x = 250;
 		}
@@ -77,28 +77,34 @@ update: function() {
 		else {
 			this.sprite.body.velocity.x = 0;
 		}
-},
+	},
 
-destroyBox: function(sprite, box) {
-	box.destroy();
-},
+	destroyBox: function(sprite, box) {
+		box.destroy();
+	},
 }
 
 
 var titleState = {
 	preload: function(){
+		//Set the background Color
 		game.stage.backgroundColor = '#000000';
 	},
 
 	create: function(){
+		//Add text with value set to "Platformer"
 		this.labelTitle = game.add.text(game.world.centerX, game.world.centerY-125, "Platformer", {font: '50px Arial', fill: '#ffffff'});
 		this.labelTitle.anchor.setTo(0.5, 0.5);
+		//Add text with value set to "Play"
 		this.labelTitle = game.add.text(game.world.centerX, game.world.centerY-25, "Play", {font: '30px Arial', fill: '#ffffff'});
 		this.labelTitle.anchor.setTo(0.5, 0.5);
+		//Allow for the "Play" text to be clicked on
 		this.labelTitle.inputEnabled = true;
+		//If "Play" is clicked on, then start "mainState"
 		this.labelTitle.events.onInputDown.add(function(){
 			game.state.start('main');
 		}, this);
+		//If spacebar is pushed, start mainState
 		var spaceKey = game.input.keyboard.addKey(
 			Phaser.Keyboard.SPACEBAR);
 		spaceKey.onDown.add(function(){
@@ -111,8 +117,11 @@ var titleState = {
 	},
 }
 
-
+//Create a new game, set the value inside the game variable
 var game = new Phaser.Game(800,600);
+//Add the mainState
 game.state.add('main', mainState);
+//Add the titleState
 game.state.add('title', titleState);
+//Start the game with the titleState
 game.state.start('title');
