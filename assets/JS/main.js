b306565f18dcfa46c1d8f49d6e380bb263fb127d
Matt <matthewmorado@gmail.com>
2017-03-08 17:19:03 -0800

    Replaced player sprite and he can now turn

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,7 +1,7 @@
 //Put the entire state into a variable
 var mainState = {
 	preload: function(){
-  		this.game.load.spritesheet('player', 'assets/images/dude.png', 32, 48);
+  		this.game.load.image('player', 'assets/images/ph_char.png');
 			this.game.load.tilemap('tilemap', 'assets/level1.json', null, Phaser.Tilemap.TILED_JSON);
  	 	this.game.load.image('tiles', 'assets/images/Tiles_64x64.png');
 			this.game.load.image('box', 'assets/images/tile_06.png');
@@ -31,6 +31,7 @@ var mainState = {
 
 		//Add the sprite to the game and enable arcade physics on it
 		this.sprite = this.game.add.sprite(50, this.game.world.centerY, 'player');
+		this.sprite.anchor.setTo(.5, 1);
 		this.game.physics.arcade.enable(this.sprite);
 
 		this.boxes = this.game.add.group();
@@ -49,8 +50,8 @@ var mainState = {
 		this.sprite.body.velocity.x = 0;
 
 		//Create a running animation for the sprite and play it
-		this.sprite.animations.add('right', [5, 6, 7, 8], 10, true);
-		this.sprite.animations.play('right');
+		//this.sprite.animations.add('right', [5, 6, 7, 8], 10, true);
+		//this.sprite.animations.play('right');
 
 		//Make the camera follow the sprite
 		this.game.camera.follow(this.sprite);
@@ -70,9 +71,11 @@ var mainState = {
 	
 		if(this.cursors.right.isDown) {
 			this.sprite.body.velocity.x = 250;
+			this.sprite.scale.setTo(1, 1);
 		}
 		else if(this.cursors.left.isDown) {
 			this.sprite.body.velocity.x = -250;
+			this.sprite.scale.setTo(-1, 1);
 		}
 		else {
 			this.sprite.body.velocity.x = 0;
