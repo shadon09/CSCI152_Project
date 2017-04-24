b028547be2a6c73d766c1f2998f591d2f20173db
Juan Ramirez <Juan Ramirez>
2017-04-24 11:59:42 -0700

    added player stuff

--- assets/JS/player.js
+++ assets/JS/player.js
@@ -1,13 +1,14 @@
+
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 //http://localhost:8080
 
 
 
-var playerHealth= 50;
+
 var healthPack;
 var myHealthBar;
 var playerLives = 5;
-
+var playerXP = 60;
 
 
 function preload() {
@@ -18,7 +19,7 @@ function preload() {
     game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 	game.load.spritesheet('baddie', 'assets/baddie.png', 32, 32);
 	game.load.image('bullet', 'assets/bullet43.png');
-	game.load.image('flame', 'assets/flame.png');
+	game.load.image('flame', 'assets/flame2.png');
 	game.load.image('healthP', 'assets/firstaid.png');
 	game.load.spritesheet('button', 'assets/plus_minus.png', 31, 31);
 	game.load.bitmapFont('myFont', 'assets/carrier_command.png', 'assets/carrier_command.xml');
@@ -36,9 +37,10 @@ function create() {
     game.add.sprite(0, 0, 'sky');
     platforms = game.add.group();
     platforms.enableBody = true;
+	reload =  game.input.keyboard.addKey(Phaser.Keyboard.R);
 	jumpButton = game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
 	cursors = game.input.keyboard.createCursorKeys();
-
+	
 
     var ground = platforms.create(0, game.world.height - 64, 'ground');
     ground.scale.setTo(2, 2);
@@ -60,11 +62,14 @@ function create() {
     player.animations.add('right', [5, 6, 7, 8], 10, true);
 	player.body.gravity.y = 1400;
 	player.body.allowRotation= false;
+	player.setHealth(50);
+	
+
 	
 	enemy1 = game.add.sprite(500, game.world.height-150,'baddie');
 	enemy1.enableBody = true;
 	enemy1.anchor.set(0.5);
-	enemy1.health =  100;
+	enemy1.setHealth(enemy1.maxHealth);
     game.physics.arcade.enable(enemy1);
     enemy1.body.bounce.y = 0.2;
     enemy1.body.collideWorldBounds = true;
@@ -73,7 +78,10 @@ function create() {
 	enemy1.body.gravity.y = 1400;
 	enemy1.body.allowRotation= false;
 	
-	myTween = game.add.tween(enemy1).to({x:300 }, 2000, 'Linear', true,0,100,true);
+	
+	enemies= game.add.group();
+	enemies.add(enemy1);
+	//myTween = game.add.tween(enemy1).to({x:300 }, 2000, 'Linear', true,0,100,true);
 	
 	var graphics = game.add.graphics(10, 10);
 	graphics.anchor.set(.5);
@@ -82,15 +90,15 @@ function create() {
 	graphics.alpha = .7;
 	graphics.endFill();
 	
-	hpText = game.add.bitmapText(graphics.position.x+10, graphics.position.y+25, 'myFont', 'HP:\n'+playerHealth+'/'+ player.maxHealth, 10);
-	xpText = game.add.bitmapText(graphics.position.x+10, graphics.position.y+50, 'myFont', 'Xp:', 10);
+	hpText = game.add.bitmapText(graphics.position.x+10, graphics.position.y+25, 'myFont', 'HP:\n'+player.health+'/'+ player.maxHealth, 10);
+	xpText = game.add.bitmapText(graphics.position.x+10, graphics.position.y+60, 'myFont', 'Xp:', 10);
 	var HbarConfig = {width: 250, height: 10, x: graphics.position.x+170, y: graphics.position.y+30, bg: {color: '#8ABA7E'}, bar:{color: '#27B902'}, animationDuration: 200, flipped: false};
-	var XPbarConfig = {width: 250, height: 10, x: graphics.position.x+170, y: graphics.position.y+55, bg: {color: '#8ABA7E'}, bar:{color: '#27B902'}, animationDuration: 200, flipped: false};
+	var XPbarConfig = {width: 250, height: 10, x: graphics.position.x+170, y: graphics.position.y+65, bg: {color: '#6DA1E3'}, bar:{color: '#2280F7'}, animationDuration: 200, flipped: false};
 	var EnemybarConfig = {width: 30, height: 5, x: enemy1.position.x, y: enemy1.position.y+25, bg: {color: '#EE4141'}, bar:{color: '#FF0000'}, animationDuration: 200, flipped: false};
 	myXPbar = new HealthBar(game, XPbarConfig);
-	myXPbar.setPercent(100);
+	myXPbar.setPercent(playerXP);
 	myHealthBar =  new HealthBar(game, HbarConfig);
-	myHealthBar.setPercent(playerHealth);
+	myHealthBar.setPercent(player.health);
 	
 	enemy1HealthBar = new HealthBar(game, EnemybarConfig);
 	enemy1HealthBar.setPercent(enemy1.health);
@@ -110,28 +118,32 @@ function create() {
 	ammoPack.anchor.set(.5);
 	*/
 	
-	
-	weapon  = game.add.weapon(30, 'bullet');
+	var myPoint = new Phaser.Point(-100,-100);
+	weapon = game.add.weapon(30, 'bullet');
 	weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
-	weapon.bulletSpeed = 700;
-	weapon.fireRate=100;
+	weapon.bulletSpeed = 900;
+	weapon.fireRate=200;
 	weapon.trackSprite(player, 0,0,false);
-	weapon.fireLimit = 200;
-	weapon.bulletGravity = -1400;
+	weapon.fireLimit = 30;
+	
+	
 	
 	
 	
 	weapon2 = game.add.weapon(30, 'flame');
 	weapon2.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
-	weapon2.bulletSpeed = 300;
-	weapon2.fireRate = 500;
+	weapon2.bulletSpeed = 900;
+	weapon2.fireRate = 30;
 	weapon2.trackSprite(player, 0,0,false);
 	weapon2.fireLimit = 200;
-
+	weapon2.bulletGravity = myPoint;
+	weapon2.bulletAngleVariance = 10;
 	
 	textGroup= game.add.group();
-	livesText = game.add.bitmapText(graphics.position.x+10,graphics.position.y+75 , 'myFont', "lives: " + playerLives, 10);
+	livesText = game.add.bitmapText(graphics.position.x+10,graphics.position.y+85 , 'myFont', "lives: " + playerLives, 10);
+	weaponInfoText =  game.add.bitmapText(graphics.position.x+10, graphics.position.y+110, 'myFont', "Ammo: " + (weapon.fireLimit -  weapon.shots) + '/'+ weapon.fireLimit, 10);
 	textGroup.add(livesText);
+	
 	game.camera.follow(player);
 	
 
@@ -143,10 +155,18 @@ function update() {
 	game.physics.arcade.collide(player, platforms);
 	game.physics.arcade.collide(enemy1, platforms);
 	game.physics.arcade.overlap(weapon.bullets, platforms, bulletPlatformCollision, null, this);
-	game.physics.arcade.overlap(weapon.bullets, enemy1, bulletEnemyCollision, null, this);
+	game.physics.arcade.overlap(weapon.bullets, enemies, bulletEnemyCollision, null, this);
+	game.physics.arcade.overlap(player, enemy1, playerEnemyCollision, null, this);
 	game.physics.arcade.collide(healthPack, platforms);
 	game.physics.arcade.overlap(player, healthPack, healthPackCollision, null, this);
-	hpText.text = 'HP:\n'+ playerHealth+'/'+ player.maxHealth;
+	
+	weaponInfoText.text = "Ammo: " + (weapon.fireLimit -  weapon.shots) + '/'+ weapon.fireLimit;
+	hpText.text = 'HP:\n'+ player.health+'/'+ player.maxHealth;
+	livesText.text = 'Lives: ' + playerLives;
+	
+	myHealthBar.setPercent(player.health);
+	myXPbar.setPercent(playerXP);
+	
 	
 	enemy1HealthBar.setPosition(enemy1.position.x, enemy1.position.y-25);
 	enemy1HealthBar.setPercent(enemy1.health);
@@ -162,12 +182,12 @@ function update() {
 		weapon.fireAngle= game.input.activePointer.position.angle(player,game.input.activePointer);
 		weapon.fireAngle+=180;
         weapon.fire();
+		console.log(weapon.shots);
     }
     if (cursors.left.isDown)
     {
         //  Move to the left
         player.body.velocity.x = -150;
-		
         player.animations.play('left');
     }
     else if (cursors.right.isDown)
@@ -190,31 +210,47 @@ function update() {
     {
         player.body.velocity.y = -500;
     }
-
-	
+	if(reload.isDown){
+		
+		weapon.resetShots(30);
+		
+	}
 	
 	
 }
+
+
+function playerEnemyCollision(player, enemy1){
+	
+	player.damage(10);
+	if(player.health <= 0){
+		playerLives--;
+		player.reset(100,100);
+	}
+	
+}
+
 
 
 function healthPackCollision(player, healthPack){
 	healthPack.kill();
-	playerHealth-=10;
-	myHealthBar.setPercent(playerHealth);
+	player.heal(10);
 	
 }
 
-function bulletPlatformCollision(weapon, platforms)
+function bulletPlatformCollision(bullet, platforms)
 {
-	weapon.kill();
+	bullet.kill();
 }
 
-function bulletEnemyCollision(weapon, enemy1)
+function bulletEnemyCollision(bullet, enemy)
 {
-	weapon.kill();
-
-	enemy1.health= enemy1.health-.01 ;
-	console.log(enemy1.health);
-}
-
-          
+	enemy.damage(10);
+	bullet.kill();
+	console.log(enemy.health);
+	if(enemy.health==0){
+		enemy1HealthBar.kill();
+		enemy.destroy();
+		playerXP+=10;
+	}
+}         
