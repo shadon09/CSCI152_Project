260d1cbe3037bcd4252322f6180b331b47c7cc16
longlivesquare <stevenduran@gmail.com>
2017-05-02 15:49:10 -0700

    Made changes to update look of level 1

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -99,8 +99,8 @@ var mainState = {
 	preload: function(){
   		this.game.load.image('player', 'assets/images/ph_char.png');
 		this.game.load.tilemap('tilemap', 'assets/level1.json', null, Phaser.Tilemap.TILED_JSON);
-		this.game.load.image('tiles', 'assets/images/Tiles_64x64.png');
-		this.game.load.image('box', 'assets/images/tile_06.png');
+		this.game.load.image('tiles', 'assets/images/sheet.png');
+		this.game.load.image('box', 'assets/images/crate.png');
 		this.game.load.spritesheet('simpleShootingEnemy', 'assets/images/ph_char.png');
 		this.game.load.spritesheet('simpleMeleeEnemy', 'assets/images/ph_char.png');
 		this.game.load.spritesheet('bullet', 'assets/images/ph_char.png', 10, 5);
@@ -113,13 +113,13 @@ var mainState = {
 		this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
 		//Change the background colour
-		this.game.stage.backgroundColor = "#a9f0ff";
+		this.game.stage.backgroundColor = "#170e7f";
 
 		//Add the tilemap and tileset image. The first parameter in addTilesetImage
 		//is the name you gave the tilesheet when importing it into Tiled, the second
 		//is the key to the asset in Phaser
 		this.map = this.game.add.tilemap('tilemap');
-		this.map.addTilesetImage('Tiles_64x64', 'tiles');
+		this.map.addTilesetImage('factory', 'tiles');
 
 		//Add both the background and ground layers. We won't be doing anything with the
 		//GroundLayer though
