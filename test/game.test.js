55f9c358ff1c8bcfdaf53199e4613ffe0dcd1ead
Matt <matthewmorado@gmail.com>
2017-05-09 14:39:48 -0700

    Fixed player size

--- test/game.test.js
+++ test/game.test.js
@@ -1,8 +1,6 @@
 describe('mainState', function() {
   describe('create', function () {
     it('There should be a player', function () {
-
-
       expect(player).to.be.not.equal(null);
     });
   });
@@ -11,8 +9,6 @@ describe('mainState', function() {
 describe('mainState', function() {
   describe('create', function () {
     it('Player health should be in the range 0 to 100', function () {
-
-
       expect(player.health).to.be.within(0,100);
     });
   });
@@ -22,8 +18,6 @@ describe('mainState', function() {
 describe('mainState', function() {
   describe('create', function () {
     it('There should be at least one melee enemy', function () {
-
-
       expect(mainState.simpleMeleeEnemies.length).to.be.above(0);
     });
   });
@@ -33,8 +27,6 @@ describe('mainState', function() {
 describe('mainState', function() {
   describe('create', function () {
     it('There should be at least one shooting enemy', function () {
-
-
       expect(mainState.simpleShootingEnemies.length).to.be.above(0);
     });
   });
